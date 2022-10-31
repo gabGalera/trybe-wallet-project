6a3e9ef6d960e1468402cf26e564aaeab4e531c5
Gabriel Galera <gabgalera@hotmail.com>
2022-10-31 16:39:38 -0300

    Validação de e-mail e redirecionamento para a carteira

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { addEmail } from '../redux/actions';
 
 class Login extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      isDisabled: true,
+      email: '',
+      password: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.type]: target.value,
+    }, () => this.handleButton());
+  };
+
+  handleButton = () => {
+    const { email, password } = this.state;
+    const minLength = 6;
+    if (email.match(/[\w]+[@]+[\w]+\.+[com]/) && password.length >= minLength) {
+      this.setState({
+        isDisabled: false,
+      });
+    } else {
+      this.setState({
+        isDisabled: true,
+      });
+    }
+  };
+
   render() {
+    const { isDisabled, email } = this.state;
+    const { dispatch } = this.props;
     return (
       <div id="main-container">
         <div id="title-container">
@@ -14,18 +50,45 @@ class Login extends React.Component {
             type="email"
             placeholder="E-mail"
             data-testid="email-input"
+            onChange={
+              this.handleChange
+            }
+            required
           />
           <input
             id="password-input"
             type="password"
             placeholder="Senha"
             data-testid="password-input"
+            onChange={
+              this.handleChange
+            }
+            required
           />
-          <button type="button">Entrar</button>
+          <Link to="/carteira">
+            <button
+              type="button"
+              disabled={ isDisabled }
+              onClick={
+                () => dispatch(addEmail(email))
+              }
+            >
+              Entrar
+
+            </button>
+          </Link>
         </form>
       </div>
     );
   }
 }
 
-export default Login;
+const mapStateToProps = (state) => ({
+  email: state.user.email,
+});
+
+Login.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+};
+
+export default connect(mapStateToProps)(Login);
