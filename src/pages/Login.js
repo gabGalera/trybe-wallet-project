1a63802a66b6893b406ec74ecab92140bc89f18e
Gabriel Galera <gabgalera@hotmail.com>
2022-10-31 15:23:52 -0300

    Estilo inicial da página de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,7 +2,19 @@ import React from 'react';
 
 class Login extends React.Component {
   render() {
-    return <div>Login</div>;
+    return (
+      <div id="main-container">
+        <div id="title-container">
+          <div />
+          <h1>Trybe Wallet</h1>
+        </div>
+        <form>
+          <input id="email-input" type="email" placeholder="E-mail" />
+          <input id="password-input" type="password" placeholder="Senha" />
+          <button type="button">Entrar</button>
+        </form>
+      </div>
+    );
   }
 }
 
