29319a655c539dd7c77baefb86870d7f0ee880ac
Gabriel Galera <gabgalera@hotmail.com>
2022-10-31 18:37:26 -0300

    Coloca alguns itens, muda um pouco o estilo e cria o reducer wallet

--- src/components/WalletForm.js
+++ src/components/WalletForm.js
@@ -3,7 +3,10 @@ import React, { Component } from 'react';
 class WalletForm extends Component {
   render() {
     return (
-      <div>WalletForm</div>
+      <form id="box-cinza">
+        <input type="number" data-testid="value-input" />
+        <input type="textarea" data-testid="description-input" />
+      </form>
     );
   }
 }
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import WalletForm from '../components/WalletForm';
 import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <WalletForm />
+      </>
     );
   }
 }
